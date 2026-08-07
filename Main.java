125d4f6b02df9fe172429cbc9600fb4553b35100
Sukhu236 <sukhu27274@gmail.com>
2026-08-07 22:57:36 +0530

    c

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        int a = 5;
-        int b = 6;
-        System.out.println(a + b);
-        System.out.println("Sukhpreet")
-    }
-}
